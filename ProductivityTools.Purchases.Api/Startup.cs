a0a2d778a604a8810319a61d85fdafbdca1c7fa8
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-10-27 23:15:12 +0100

    cors

--- ProductivityTools.Purchases.Api/Startup.cs
+++ ProductivityTools.Purchases.Api/Startup.cs
@@ -60,11 +60,7 @@ namespace ProductivityTools.Purchases.Api
          
             app.UseHttpsRedirection();
             app.UseRouting();
-            app.UseCors(x => x
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .SetIsOriginAllowed(origin => true) // allow any origin
-            .AllowCredentials()); // allow credentials
+            app.UseCors(policy);
 
             app.UseAuthorization();
             app.UseAuthentication();
