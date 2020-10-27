285102d4a9ad073d3b7c3bc4dde23fceb79782e0
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-10-27 21:56:38 +0100

    cors

--- ProductivityTools.Purchases.Api/Startup.cs
+++ ProductivityTools.Purchases.Api/Startup.cs
@@ -32,7 +32,7 @@ namespace ProductivityTools.Purchases.Api
                 options.AddPolicy(policy,
                     builder =>
                     {
-                        builder.WithOrigins("http://localhost:3000");
+                        builder.WithOrigins("http://localhost:3000").AllowAnyHeader();
                     });
             });
 
